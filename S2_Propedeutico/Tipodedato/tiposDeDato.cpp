5f0dee1cfa0dcba3353430103c921ae37505eea2
GuadalupeRTr <guadalupe_reyes@ciencias.unam.mx>
2022-02-27 22:14:02 -0600

    Primer commit. Agregados codigos fuente de helloWorld y tipos de dato

---
+++ S2_Propedeutico/Tipodedato/tiposDeDato.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Tamaño en bytes de <int>: " << sizeof(char) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(bool) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(int) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(float) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(long) <<endl;
+    cout << "Tamaño en bytes de <int>: " << sizeof(double) <<endl;
+
+    return 0;
+}

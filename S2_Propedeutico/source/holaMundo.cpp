7634184e8de03c687f5317c29f311c48395cf530
GuadalupeRTr <guadalupe_reyes@ciencias.unam.mx>
2022-03-07 01:24:22 -0600

    Se borra carpeta output para que funcione gitignore

--- S2_Propedeutico/source/holaMundo.cpp
+++ S2_Propedeutico/source/holaMundo.cpp
@@ -6,27 +6,4 @@ int main(){
     //std::cout << "Hola, mundo!" << std::endl;
     cout << "Hola, mundo!" << endl;     
     return 0;
-<<<<<<< HEAD
-=======
-
->>>>>>> rama1
-<<<<<<< HEAD
-    //arreglando codigo
-=======
-    //modificación para realizar rama secundaria para hacer merge
-
-<<<<<<< HEAD
->>>>>>> rama1
-=======
-    
->>>>>>> rama1
 }
-
-
-
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> rama1

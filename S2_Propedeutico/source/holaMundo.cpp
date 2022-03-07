b816d105b1e0697a71e227f4b8c2b106cb3f86fe
GuadalupeRTr <guadalupe_reyes@ciencias.unam.mx>
2022-03-07 00:36:25 -0600

    cambio codigo en holaMundo rama1

--- S2_Propedeutico/source/holaMundo.cpp
+++ S2_Propedeutico/source/holaMundo.cpp
@@ -6,8 +6,17 @@ int main(){
     //std::cout << "Hola, mundo!" << std::endl;
     cout << "Hola, mundo!" << endl;     
     return 0;
+
+<<<<<<< HEAD
+    //arreglando codigo
+=======
     //modificación para realizar rama secundaria para hacer merge
 
+    
 }
+
+
+
+
 
 
